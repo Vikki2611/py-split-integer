93e5d9904dc6c3ce0ab144900acdb74939904da3
Victoria <nopasaran85@gmail.com>
2026-04-01 12:23:35 +0300

    Recently fixed solution

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,26 +2,30 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    pass
-    result = split_integer(17, 4)
-    assert sum(result) == 17
+    assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    result = split_integer(6, 2)
-    assert result == [3, 3]
+    assert split_integer(6, 2) == [3, 3]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    result = split_integer(8, 1)
-    assert result == [8]
+    assert split_integer(8, 1) == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    result = split_integer(17, 4)
-    assert result == sorted(result)
+    assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    result = split_integer(3, 5)
-    assert result == [0, 0, 1, 1, 1]
+    assert split_integer(3, 5) == [0, 0, 1, 1, 1]
+
+
+def test_should_return_list_with_length_equal_to_number_of_parts() -> None:
+    assert len(split_integer(10, 3)) == 3
+
+
+def test_difference_between_max_and_min_should_be_no_more_than_one() -> None:
+    assert (max(
+        split_integer(10, 3)
+    ) - min(split_integer(10, 3)) <= 1)
